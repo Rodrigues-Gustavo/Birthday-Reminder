a27c6a3d6fdebd922b0c25948238a7443cc59d09
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-27 12:18:36 -0300

    structuring how the project will be

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 90vw;
+  margin: 5rem 0;
+  max-width: var(1170px);
+  background: var(#fff);
+  border-radius: var(0.25rem);
+  padding: 1.5rem 2rem;
+  box-shadow: var(0 5px 15px rgba(0, 0, 0, 0.4));
+  background-color: #ddff00;
+`;
 
 const App = () => {
   return (
-    <div>Birthday Reminder</div>
+    <Container>
+      <h1>Do not forget</h1>
+      <h3>1 birthday today</h3>
+      <div>birthday List</div>
+      <button>Clear List</button>
+    </Container>
   )
 }
 
