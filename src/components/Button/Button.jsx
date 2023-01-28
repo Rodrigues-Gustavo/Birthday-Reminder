8f404f1bbf6b33bcdd57bfc489dfaadd4b1b4c8d
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-28 11:57:21 -0300

    creating the butao component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+    color: #fff;
+    display: block;
+    width: 100%;
+    border-color: transparent;
+    background: #ddff00;
+    margin: 2rem auto 0 auto;
+    text-transform: capitalize;
+    font-size: 1.2rem;
+    padding: 0.5rem 0;
+    letter-spacing: 0.1rem;
+    border-radius: 0.25rem;
+    outline: 1px solid #ddff00;
+    cursor: pointer;
+`;
+
+export default Button
