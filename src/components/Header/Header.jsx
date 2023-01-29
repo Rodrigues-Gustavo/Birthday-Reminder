73a15f607bd8a4f12c7dc8d77cfbebbe92b00d3a
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-29 16:16:08 -0300

    creating component for the header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  letter-spacing: 0.1rem;
+  text-transform: capitalize;
+  line-height: 1.25;
+  margin-bottom: 0.75rem;
+  font-size: 3rem;
+
+  h1 { 
+    font-size: 3rem; 
+  }
+  
+  h3 { 
+    font-size: 1.25rem;
+  }
+
+`;
+
+export default Header
