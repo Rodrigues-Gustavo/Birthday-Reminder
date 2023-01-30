33daa7f4ee1327755a58249afd9a7b09658ee2cd
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-30 11:34:03 -0300

    creating style component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 0.75rem;
+  margin-bottom: 1.5rem;
+  align-items: center;
+`;
+
+const PersonPicture = styled.img`
+    width: 75px;
+    height: 75px;
+    object-fit: cover;
+    border-radius: 50%;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+`;
+
+const PersonInfo = styled.div`
+    font-size: 0.875rem;
+`;
+
+
+  
+
 
 const List = ({ birthdayList }) => {
   return (
@@ -7,13 +32,10 @@ const List = ({ birthdayList }) => {
             const { id, name, age, image } = person
 
             return (
-                <article key={id}>
-                    <img src={image} alt={name} />
-                    <div>
-                        <h4>{name}</h4>
-                        <p>{age}</p>
-                    </div>
-                </article>
+                <Container key={id}>
+                    <PersonPicture src={image} alt={name} />
+                    <PersonInfo>{name}, {age} years</PersonInfo>
+                </Container>
             )
         })}
     </>
