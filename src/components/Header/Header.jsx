c412461cd3ab123f50c8ebcf85d9c2cc6cae06fb
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-01-31 12:07:18 -0300

    centering texts and changing font size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,18 @@ const Header = styled.div`
   text-transform: capitalize;
   line-height: 1.25;
   margin-bottom: 0.75rem;
-  font-size: 3rem;
+  text-align: center;
+  line-height: 1.5rem;
+  
 
   h1 { 
-    font-size: 3rem; 
+    font-size: 3rem;
+    font-size : 1.25rem;
   }
   
   h3 { 
     font-size: 1.25rem;
+    font-size: 0.75rem;
   }
 
 `;
